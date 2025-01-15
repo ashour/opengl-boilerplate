ca5682a9241ac352a38b93fab040674628f76185
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-01-15 11:56:07 +0100

    style: add clang-format file

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,32 @@
 #include <OpenGL/OpenGL.h>
 #include <iostream>
 
-int main() {
-  if (!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW" << std::endl;
-    return -1;
-  }
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
 
-  auto window =
-      glfwCreateWindow(960, 540, "OpenGL Boilerplate", nullptr, nullptr);
-  if (!window) {
-    std::cerr << "Failed to create GLFW window" << std::endl;
+    auto window = glfwCreateWindow(960, 540, "OpenGL Boilerplate", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
     glfwTerminate();
-    return -1;
-  }
-
-  glfwMakeContextCurrent(window);
-
-  while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
+    return 0;
 }
