6fe96467ed87f651a5393e8fc8dfb8e7fbdeaa72
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-01-15 11:39:00 +0100

    chore: add glfw for cross-platform windowing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+#endif
+#include <GLFW/glfw3.h>
+#include <OpenGL/OpenGL.h>
 #include <iostream>
 
 int main() {
-  std::cout << "Hello boilerplate!" << std::endl;
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
+
+  auto window =
+      glfwCreateWindow(960, 540, "OpenGL Boilerplate", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return 0;
 }
