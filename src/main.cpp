258aa55d4b63ef3ca618a46263c6c40000d9b8e4
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-01-15 16:39:34 +0100

    feat: draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,28 @@
 #include <iostream>
 #include <ostream>
 
+const char *vertex_shader_source = R"(
+#version 410 core
+
+layout (location = 0) in vec3 i_position;
+
+void main()
+{
+  gl_Position = vec4(i_position.x, i_position.y, i_position.z, 1.0);
+}
+)";
+
+const char *fragment_shader_source = R"(
+#version 410 core
+
+out vec4 o_color;
+
+void main()
+{
+  o_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)";
+
 int main()
 {
     if (!glfwInit())
@@ -13,7 +35,16 @@ int main()
         return -1;
     }
 
-    auto window = glfwCreateWindow(960, 540, "OpenGL Boilerplate", nullptr, nullptr);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    std::cout << "Apple env detected" << std::endl;
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(960, 540, "OpenGL Boilerplate", nullptr, nullptr);
     if (!window)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -26,13 +57,79 @@ int main()
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
         return -1;
     }
+
+    std::cout << "OpenGL version " << glGetString(GL_VERSION) << std::endl;
+
+    // clang-format off
+    float vertices[] = {
+        0.0f,  0.5f, 0.0f,
+       -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+    // clang-format on
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, nullptr);
+    glCompileShader(vertex_shader);
+
+    int success;
+    char info_log[512];
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertex_shader, 512, nullptr, info_log);
+        std::cerr << "Failed to compile vertex shader: " << info_log << std::endl;
+    }
+
+    unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, nullptr);
+    glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragment_shader, 512, nullptr, info_log);
+        std::cerr << "Failed to compile fragment shader: " << info_log << std::endl;
+    }
+
+    unsigned int shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shader_program, 512, nullptr, info_log);
+        std::cerr << "Failed to attach or link shaders to program: " << info_log << std::endl;
+    }
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
 
