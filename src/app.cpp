8d08e34665238dcf0c5e3a6ce3dd5bbfea2f848a
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-01-27 22:47:57 +0100

    feat: update light orbit

--- src/app.cpp
+++ src/app.cpp
@@ -73,9 +73,9 @@ void App::init_rendering()
     _light =
         std::make_unique<DirectionalLight>(*_shader,
                                            "u_directional_light",
-                                           glm::vec3(-2.0f, -0.1f, -0.3f),
+                                           glm::vec3(0.0f),
+                                           glm::vec3(0.35f),
                                            glm::vec3(0.4f),
-                                           glm::vec3(0.6f),
                                            glm::vec3(EO_NCOLV(255), EO_NCOLV(204), EO_NCOLV(107)));
 
     _mat_dirt = std::make_unique<Material>(*_shader, TEXTURE_DIR + "dirt.png", Format::RGBA, 25.0f);
@@ -157,10 +157,11 @@ void App::render_scene()
     _shader->set_uniform_vec3(_u_view_position, _camera->position());
 
     constexpr float light_orbit_radius = 50.0f;
+    float light_y_tilt = glm::tan(glm::radians(-30.0f));
     float light_angle = std::fmod(glm::radians(Time::current_time() * 20.0f), glm::two_pi<float>());
     _light->set_direction(glm::vec3(light_orbit_radius * glm::cos(light_angle),
-                                    light_orbit_radius * glm::sin(light_angle),
-                                    0.0f));
+                                    light_orbit_radius * light_y_tilt,
+                                    light_orbit_radius * glm::sin(light_angle)));
 
     int u_model = _shader->uniform_location_for("u_model");
     int u_texture_scale = _shader->uniform_location_for("u_texture_scale");
