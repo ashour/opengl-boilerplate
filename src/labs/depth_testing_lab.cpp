47f7e771218914316f21ccdbfe97ac6a9a35a5d5
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-02-05 14:13:52 +0100

    feat: depth testing

---
+++ src/labs/depth_testing_lab.cpp
@@ -0,0 +1,89 @@
+#include "config.h"
+#include "depth_testing_lab.h"
+#include "lib/random.h"
+#include "objects/primitive.h"
+#include "rendering/transform.h"
+#include "system/input.h"
+#include "system/time.h"
+
+namespace eo
+{
+
+DepthTestingLab::DepthTestingLab(const Window& window) : Lab(window)
+{
+    gldc(glDepthFunc(GL_LESS));
+    _window.set_clear_color(SCENE_CLEAR_COLOR);
+
+    _shader = std::make_shared<Shader>("resources/shaders/unlit_texture.vert",
+                                       "resources/shaders/unlit_texture.frag");
+    _shader->build();
+
+    _shader->use();
+    _u_view_matrix = _shader->uniform_location_for("u_view");
+
+    _camera = std::make_unique<Camera>(
+        static_cast<float>(_window.buffer_width() / static_cast<float>(_window.buffer_height())));
+
+    _shader->set_uniform("u_projection", _camera->projection());
+
+    Shader::unuse_all();
+
+    _plane = std::make_unique<Mesh>(Primitive::plane(), nullptr);
+
+    _cube = std::make_unique<Mesh>(Primitive::cube(), nullptr);
+
+    for (size_t i = 0; i < _cube_positions.size(); i += 1)
+    {
+        _cube_positions[i] = {
+            random_float(-90.0f, 90.0f),
+            random_float(2.0f, 20.0f),
+            random_float(-90.0f, 90.0f),
+        };
+    }
+
+    Input::register_mouse_move_handler(
+        [this](auto current_mouse_position, auto last_mouse_position)
+        { _camera->look(current_mouse_position, last_mouse_position); });
+}
+
+void DepthTestingLab::OnUpdate()
+{
+    if (Input::action_pressed(Action::move_forward))
+    {
+        _camera->strafe(Strafe::forward, Time::delta_time());
+    }
+    if (Input::action_pressed(Action::move_back))
+    {
+        _camera->strafe(Strafe::back, Time::delta_time());
+    }
+    if (Input::action_pressed(Action::move_left))
+    {
+        _camera->strafe(Strafe::left, Time::delta_time());
+    }
+    if (Input::action_pressed(Action::move_right))
+    {
+        _camera->strafe(Strafe::right, Time::delta_time());
+    }
+}
+
+void DepthTestingLab::OnRender()
+{
+    _shader->use();
+    _shader->set_uniform(_u_view_matrix, _camera->view());
+
+    Transform cube_transform{};
+    cube_transform.rotation(Time::current_time() * glm::radians(50.0f), {0.5f, 1.0f, 0.0f});
+    for (glm::vec3 position : _cube_positions)
+    {
+        cube_transform.position(position);
+        _shader->set_uniform("u_model", cube_transform.matrix());
+        _cube->draw();
+    }
+
+    Transform plane_transform{};
+    plane_transform.scale(glm::vec3(200.0f, 1.0f, 200.0f));
+    _shader->set_uniform("u_model", plane_transform.matrix());
+    _plane->draw();
+}
+
+} // namespace eo
---
+++ src/labs/depth_testing_lab.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "labs/lab.h"
+#include "rendering/camera.h"
+#include "rendering/mesh.h"
+#include "rendering/shader.h"
+#include "system/window.h"
+#include <memory>
+
+namespace eo
+{
+class DepthTestingLab : public Lab
+{
+  public:
+    DepthTestingLab(const Window& window);
+
+    void OnUpdate() override;
+    void OnRender() override;
+
+  private:
+    std::unique_ptr<Camera> _camera;
+    std::shared_ptr<Shader> _shader;
+
+    int _u_view_matrix;
+
+    std::unique_ptr<Mesh> _cube;
+    std::array<glm::vec3, 180> _cube_positions{};
+
+    std::unique_ptr<Mesh> _plane;
+};
+} // namespace eo
