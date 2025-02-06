385137553ce9937f660f580a090978eb913dc6df
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-02-06 17:32:08 +0100

    feat: alpha blending

--- src/labs/blending_lab.cpp
+++ src/labs/blending_lab.cpp
@@ -5,6 +5,7 @@
 #include "rendering/transform.h"
 #include "system/input.h"
 #include "system/time.h"
+#include <map>
 
 namespace eo
 {
@@ -22,6 +23,10 @@ BlendingLab::BlendingLab(const Window& window) : Lab(window)
                                  "resources/shaders/discard_transparent_texture.frag");
     _discard_transparent_tex_shader->build();
 
+    _unlit_alpha_texture = std::make_shared<Shader>("resources/shaders/unlit_texture.vert",
+                                                    "resources/shaders/unlit_alpha_texture.frag");
+    _unlit_alpha_texture->build();
+
     _camera = std::make_unique<Camera>(
         static_cast<float>(_window.buffer_width() / static_cast<float>(_window.buffer_height())));
 
@@ -32,6 +37,10 @@ BlendingLab::BlendingLab(const Window& window) : Lab(window)
     _discard_transparent_tex_shader->use();
     _u_view_matrix = _discard_transparent_tex_shader->uniform_location_for("u_view");
     _discard_transparent_tex_shader->set_uniform("u_projection", _camera->projection());
+
+    _unlit_alpha_texture->use();
+    _u_view_matrix = _unlit_alpha_texture->uniform_location_for("u_view");
+    _unlit_alpha_texture->set_uniform("u_projection", _camera->projection());
 
     Shader::unuse_all();
 
@@ -64,7 +73,16 @@ BlendingLab::BlendingLab(const Window& window) : Lab(window)
         Texture::no_specular(),
     };
     _mat_grass = std::make_shared<Material>(mat_grass_textures);
-    _quad = std::make_unique<Mesh>(Primitive::quad(), _mat_grass);
+    _grass = std::make_unique<Mesh>(Primitive::quad(), _mat_grass);
+
+    std::vector<std::shared_ptr<Texture>> mat_window_textures{
+        std::make_shared<Texture>(Texture::Type::diffuse,
+                                  "resources/textures/blending_transparent_window.png",
+                                  Texture::Wrap::clamp_to_edge),
+        Texture::no_specular(),
+    };
+    _mat_window = std::make_shared<Material>(mat_window_textures);
+    _transparent_window = std::make_unique<Mesh>(Primitive::quad(), _mat_window);
 
     Input::register_mouse_move_handler(
         [this](auto current_mouse_position, auto last_mouse_position)
@@ -118,17 +136,43 @@ void BlendingLab::on_render()
     }
     _mat_box->unbind(*_unlit_tex_shader);
 
+    gldc(glDisable(GL_CULL_FACE));
     _discard_transparent_tex_shader->use();
     _discard_transparent_tex_shader->set_uniform(_u_view_matrix, _camera->view());
-
-    Transform quad_transform{};
-    quad_transform.rotation(glm::radians(90.0f), {1, 0, 0});
-    quad_transform.position({0, 6.0f, 0});
-    _discard_transparent_tex_shader->set_uniform("u_model", quad_transform.matrix());
+    Transform grass_transform{};
+    grass_transform.rotation(glm::radians(90.0f), {1, 0, 0});
+    grass_transform.position({0, 1.0f, 0});
+    grass_transform.scale(glm::vec3(2.0f));
+    _discard_transparent_tex_shader->set_uniform("u_model", grass_transform.matrix());
     _discard_transparent_tex_shader->set_uniform("u_texture_scale", 1.0f);
     _mat_grass->bind(*_discard_transparent_tex_shader);
-    _quad->draw();
+    _grass->draw();
     _mat_grass->unbind(*_discard_transparent_tex_shader);
+
+    gldc(glEnable(GL_BLEND));
+    gldc(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+    _unlit_alpha_texture->use();
+    _unlit_alpha_texture->set_uniform(_u_view_matrix, _camera->view());
+    _unlit_alpha_texture->set_uniform("u_texture_scale", 1.0f);
+    Transform window_transform{};
+    window_transform.rotation(glm::radians(90.0f), {1, 0, 0});
+
+    std::map<float, glm::vec3> sorted_windows;
+    for (glm::vec3 window_position : _window_positions)
+    {
+        float distance = glm::length(_camera->position() - window_position);
+        sorted_windows[distance] = window_position;
+    }
+    for (auto it = sorted_windows.rbegin(); it != sorted_windows.rend(); it++)
+    {
+        window_transform.position(it->second);
+        _unlit_alpha_texture->set_uniform("u_model", window_transform.matrix());
+        _mat_window->bind(*_unlit_alpha_texture);
+        _transparent_window->draw();
+        _mat_window->unbind(*_unlit_alpha_texture);
+    }
+    gldc(glDisable(GL_BLEND));
+    gldc(glEnable(GL_CULL_FACE));
 }
 
 } // namespace eo
--- src/labs/blending_lab.h
+++ src/labs/blending_lab.h
@@ -23,10 +23,12 @@ class BlendingLab : public Lab
     std::unique_ptr<Camera> _camera;
     std::shared_ptr<Shader> _unlit_tex_shader;
     std::shared_ptr<Shader> _discard_transparent_tex_shader;
+    std::shared_ptr<Shader> _unlit_alpha_texture;
 
     std::shared_ptr<Material> _mat_dirt;
     std::shared_ptr<Material> _mat_box;
     std::shared_ptr<Material> _mat_grass;
+    std::shared_ptr<Material> _mat_window;
 
     int _u_view_matrix;
 
@@ -35,6 +37,13 @@ class BlendingLab : public Lab
 
     std::unique_ptr<Mesh> _ground;
 
-    std::unique_ptr<Mesh> _quad;
+    std::unique_ptr<Mesh> _grass;
+    std::unique_ptr<Mesh> _transparent_window;
+    std::array<glm::vec3, 4> _window_positions{
+        glm::vec3{-10.0f, 4.0f, 0.25f},
+        glm::vec3{-10.5f, 4.5f, 0.0f},
+        glm::vec3{-7.5f, 2.0f, 0.0f},
+        glm::vec3{-8.0f, 1.5f, -0.25f},
+    };
 };
 } // namespace eo
