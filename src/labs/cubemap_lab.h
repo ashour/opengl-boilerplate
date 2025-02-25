6061b1691a1d0268e3fbdfc92c1fafad32368975
Mohammad Ashour <ashour.mohammad@gmail.com>
2025-02-25 15:41:45 +0000

    clean: remove unused constants

--- src/labs/cubemap_lab.h
+++ src/labs/cubemap_lab.h
@@ -23,17 +23,11 @@ class CubemapLab : public Lab
     void on_ui_render(UI& ui) override;
 
   private:
-    const float REFRACTIVE_INDEX_AIR = 1.00f;
-    const float REFRACTIVE_INDEX_WATER = 1.33f;
-    const float REFRACTIVE_INDEX_ICE = 1.309f;
-    const float REFRACTIVE_INDEX_GLASS = 1.52f;
-    const float REFRACTIVE_INDEX_DIAMOND = 2.42f;
-
     std::unique_ptr<Camera> _camera;
     std::shared_ptr<Shader> _skybox_reflection_shader;
     std::shared_ptr<Shader> _skybox_refraction_shader;
 
-    float _refractive_index{1.00};
+    float _refractive_index{1.00f};
 
     std::shared_ptr<Material> _mat_box;
 
@@ -45,6 +39,7 @@ class CubemapLab : public Lab
     std::unique_ptr<Mesh> _skybox;
     std::shared_ptr<Shader> _skybox_shader;
     unsigned int _skybox_texture;
+
     unsigned int load_cubemap(std::vector<const std::string>& face_texture_filepaths);
 };
 } // namespace eo
